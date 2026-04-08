033f15cbbf26bcafe764d23ea3b77e0dccce53e1
simib-ai <simi.b@allynutra.com>
2026-04-08 12:13:05 -0300

    fix basename

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/offer-ally-nutra">
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/quote" element={<Quote />} />
