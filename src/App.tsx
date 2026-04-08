d57dad3128090d773391f6726692822372c6f3ef
simib-ai <simi.b@allynutra.com>
2026-04-08 12:39:36 -0300

    fix: router

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter basename="/offer-ally-nutra">
+      <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/quote" element={<Quote />} />
